94e017081a2c08bb9bf3b04e96a88ffd33c1e326
fionapigott <fpigott@twitter.com>
2016-08-05 10:53:37 -0600

    works for string Tweets IDs not ints

--- snowflake2utc.py
+++ snowflake2utc.py
@@ -3,4 +3,5 @@
 # Nick Galbreath @ngalbreath nickg@client9.com
 # Public Domain -- No Copyright -- Cut-n-Paste!
 def snowflake2utc(sf):
-    return int(((sf >> 22) + 1288834974657) / 1000.0)
+    sf_int = int(sf)
+    return int(((sf_int >> 22) + 1288834974657) / 1000.0)
